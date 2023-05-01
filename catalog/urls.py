e688ba4ae110312569430092123c63245b7a7a5e
Roman Pysarskiy <113036822+Soobig666@users.noreply.github.com>
2023-05-01 14:00:27 +0300

    Add basepage & homepage & nav-panel & footer

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 
+from catalog.views import (
+    index,
+    WorkersListView,
+
+)
+
 urlpatterns = [
+    path("", index, name="index"),
+    path("workers/", WorkersListView.as_view(), name="worker")
 ]
 
 app_name = 'catalog'
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from catalog.models import Worker, Task, TaskType, Position
+
+
+# @login_required
+def index(request):
+    num_task = Task.objects.count()
+    num_worker = Worker.objects.count()
+    num_position = Position.objects.count()
+    num_task_type = TaskType.objects.count()
+
+    # num_visits = request.session.get ("num_visits", 0)
+    # request.session["num_visits"] = num_visits
+    context = {
+        "num_task": num_task,
+        "num_worker": num_worker,
+        "num_position": num_position,
+        "num_task_type": num_task_type,
+    }
+    return render(request, "home/index.html", context=context)
+
+
+class WorkersListView(generic.ListView):
+    template_name = "home/workers"
+
