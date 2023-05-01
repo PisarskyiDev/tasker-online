df4774d4292fa904ec78e0fe316552b9e5b4321c
Roman Pysarskiy <113036822+Soobig666@users.noreply.github.com>
2023-05-01 14:13:32 +0300

    Upd, change context for homepage

--- catalog/views.py
+++ catalog/views.py
@@ -8,6 +8,8 @@ from catalog.models import Worker, Task, TaskType, Position
 # @login_required
 def index(request):
     num_task = Task.objects.count()
+    num_task_solved = Task.objects.filter(is_completed=True).count()
+    num_task_actual = Task.objects.filter(is_completed=False).count()
     num_worker = Worker.objects.count()
     num_position = Position.objects.count()
     num_task_type = TaskType.objects.count()
@@ -19,6 +21,8 @@ def index(request):
         "num_worker": num_worker,
         "num_position": num_position,
         "num_task_type": num_task_type,
+        "num_task_solved": num_task_solved,
+        "num_task_actual": num_task_actual,
     }
     return render(request, "home/index.html", context=context)
 
