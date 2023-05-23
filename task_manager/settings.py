b568955fbe7997a0d5bc8679836a9cd96b582a48
soobig666 <darkvader.ukr.net@gmail.com>
2023-05-23 11:24:34 +0300

    Fix secret key can't be empty, key need for tests

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -26,6 +26,7 @@ CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv(
     "DJANGO_SECRET_KEY",
+    "can't be empty because test not passed"
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
