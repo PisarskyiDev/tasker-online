5a53afe9ff4bcd9d664097f7cbd5f092430f747f
soobig666 <darkvader.ukr.net@gmail.com>
2023-08-29 16:20:02 +0300

    Upd settings 'social_django' in settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -147,23 +147,29 @@ AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
 )
 
+# –––––– CLEAN_USERNAMES = False
+
 SOCIAL_AUTH_PIPELINE = (
     "social_core.pipeline.social_auth.social_details",
     "social_core.pipeline.social_auth.social_uid",
     "social_core.pipeline.social_auth.auth_allowed",
     "social_core.pipeline.social_auth.social_user",
-    "task_manager.pipeline.check_username",
+    "social_core.pipeline.social_auth.associate_by_email",
     "social_core.pipeline.user.get_username",
     "social_core.pipeline.user.create_user",
     "social_core.pipeline.social_auth.associate_user",
     "social_core.pipeline.social_auth.load_extra_data",
     "social_core.pipeline.user.user_details",
 )
-# CLEAN_USERNAMES = False
+
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv("GOOGLE_CLIENT_ID")
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+# SOCIAL_AUTH_PASSWORDLESS = False
+SOCIAL_AUTH_REVOKE_TOKENS_ON_DISCONNECT = False
+SOCIAL_AUTH_USER_MODEL = "user.Worker"
+SOCIAL_AUTH_IMMUTABLE_USER_FIELDS = ["username", "first_name", "last_name"]
 
-# End social_django settings
+# –––––– End social_django settings
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
