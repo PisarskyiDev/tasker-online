9b5388f1287c333121843785d9740e21742507fa
soobig666 <darkvader.ukr.net@gmail.com>
2023-09-13 14:25:27 +0300

    Del old "initial" & Add new, migrate && Fix problem in admin panel with "date_joined" (add to readonly)

--- user/admin.py
+++ user/admin.py
@@ -20,7 +20,7 @@ class WorkerAdmin(DjangoUserAdmin):
             _("Permissions"),
             {
                 "fields": (
-                    "username" "is_active",
+                    "is_active",
                     "is_staff",
                     "is_superuser",
                     "groups",
@@ -39,6 +39,8 @@ class WorkerAdmin(DjangoUserAdmin):
             },
         ),
     )
-    list_display = ("username", "email", "first_name", "last_name", "is_staff")
-    search_fields = ("username", "email", "first_name", "last_name")
+
+    readonly_fields = ("date_joined",)
+    list_display = ("username", "email", "last_name", "is_staff")
+    search_fields = ("username", "email")
     ordering = ("email",)
